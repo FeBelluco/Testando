452fd3646cb81dc2741dd146e67232e74e1d73d6
Felipe Belluco <felipe.loureiro@alphasistemasti.com.br>
2024-12-17 17:00:57 -0300

    test2

---
+++ styles.ts
@@ -0,0 +1,32 @@
+import styled from "styled-components/native";
+
+export const StatusView = styled.View`
+  width: 100%;
+  height: 20%;
+  position: relative;
+
+  justify-content: center;
+
+  border-bottom-left-radius: 14px;
+  border-bottom-right-radius: 14px;
+  background-color: #ffb800;
+`;
+
+export const IconView = styled.View`
+  width: 35px;
+  height: 35px;
+
+  left: 85%;
+  justify-content: center;
+
+  bottom: 10px;
+
+  border-radius: 25px;
+  border-width: 1px;
+  border-color: white;
+  background-color: #ffb800;
+`;
+export const TextView = styled.View`
+  overflow: hidden;
+  top: 16px;
+`;
